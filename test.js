54aaa0807b48f6e24f5ee772337689bd879e06f9
ABLEN93 <iamme5643@gmail.com>
2021-04-12 14:58:12 +0200

    mywork

---
+++ test.js
@@ -0,0 +1,26 @@
+var X = '3';
+var Y = "3";
+if (X == Y) {
+    alert("True");
+} else { alert("False")  
+};
+
+
+// the Answer its right , no diffrence cuz the Value is the same, we have to use ===
+// if we use = its still True
+
+var X = '3';
+var Y = "3";
+if (X = Y) {
+    alert("True");
+} else { alert("False")  
+};
+
+// 2 here its showing False
+var X = 'True';
+var Y = "False";
+if (X === Y) {
+    alert("True");
+} else { alert("False")  
+};
+// its will show stacy
